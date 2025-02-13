2718f23c940d547fdf293a2e19312a39075a69d7
Huzaifa1711 <mian.huzaifatariq@gmail.com>
2025-02-13 14:21:33 +0500

    Fixed Pipenv installation in build script

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+
+# Install Pipenv if not installed
+pip install pipenv
+
+# Install dependencies using Pipenv
+pipenv install --system
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'expenseswebsite/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
