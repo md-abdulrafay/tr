9a80e1d05f109a293366fbcb7476a355ea655a7c
Huzaifa1711 <mian.huzaifatariq@gmail.com>
2025-02-17 18:25:44 +0500

    all psal

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_USER_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'incomeexpensedb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
