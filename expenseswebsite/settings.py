79557b2121d90461cc9bca6cbcb7de135aa62ff8
Huzaifa1711 <mian.huzaifatariq@gmail.com>
2025-02-17 11:57:47 +0500

    fix engine

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -83,15 +83,14 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('DB_NAME'),
-    #     'USER': os.getenv('DB_USER'),
-    #     'PASSWORD': os.getenv('DB_USER_PASSWORD'),  # Add your database password here
-    #     'HOST': os.getenv('DB_HOST'),
-    #     'PORT': os.getenv('DB_PORT'),     
-    # }
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_USER_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 # Add this setting to configure the default auto field
